20936d2e944eb64b6595d186bd9264277b53d176
KauanAndrade57 <168490759+KauanAndrade57@users.noreply.github.com>
2024-11-21 11:52:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Fortnite',
+    'O que vale mais, jogar seu amigo do helicoptero ou ganhar a partida?',
+    'Obvio que é jogar seu amigo do helicoptero.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Mbappe'
+)
+
+criaCartao(
+    'Programação',
+    'Drake, cadê a porta?',
+    'Vai ser ai ué'
+)
+
+criaCartao(
+    'Lingua Arábe',
+    'ما هو اسمي؟?',
+    'هايزنبرغ'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o melhor centroavante do Brasil em 2024?',
+    'Yuri Alberto, jogador do corinthians, 9 do Hexa.'
+)
+criaCartao(
+    'Brawl Stars',
+    'Qual o brawler que apoia a causa lgbt no Brawl Stars?',
+    'Edgar.'
+)
